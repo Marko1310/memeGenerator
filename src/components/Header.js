8be89779b8fb38b3bce019eacf7914fe9ec2188f
Marko1310 <marko.cabo13@gmail.com>
2022-12-03 23:17:14 +0100

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import troll from "../images/trollFace.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img className="header--img" src={troll} alt="meme"></img>
+      <h1 className="header--title">Meme Generator</h1>
+      <p className="header--desc">React Curse - Project</p>
+    </header>
+  );
+};
+
+export default Header;
