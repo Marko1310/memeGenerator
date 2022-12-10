0a02adf26bccdd4b3023392951e19b80280c6839
Marko1310 <marko.cabo13@gmail.com>
2022-12-10 03:08:13 +0100

    Implemented: show all images, onclick change the style of the image, add text only on the clicked images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Container from "./Container";
 
 const Meme = () => {
   // state for each meme image with top and bottom text
   const [meme, setMeme] = useState({
-    topText: "",
-    bottomText: "",
+    topText: [],
+    bottomText: [],
     randomImage: "https://i.imgflip.com/1bh3.jpg",
   });
 
   // state for all memes (array)
   const [allMemeImages, setAllMemeImages] = useState([]);
 
-  // fetch data on first render
+  // fetch data on first render, update every object with new key/value property 'clicked'
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
-      .then((data) => setAllMemeImages(data.data.memes));
+      .then((data) => {
+        const upgradedObjects = data.data.memes.map((el) => {
+          return { ...el, clicked: false };
+        });
+        console.log(upgradedObjects);
+        return setAllMemeImages(upgradedObjects);
+      });
   }, []);
 
   // erase the text, change the state of the meme image by randomly choosing an image from the array
   let getMemeImage = function () {
-    meme.topText = "";
-    meme.bottomText = "";
+    setMeme((meme) => {
+      return { ...meme, topText: [], bottomText: [] };
+    });
 
     const length = allMemeImages.length;
     const num = Math.floor(Math.random() * length);
@@ -30,6 +38,22 @@ const Meme = () => {
     setMeme((meme) => {
       return { ...meme, randomImage: allMemeImages[num].url };
     });
+  };
+
+  // function when a box is clicked
+  const selectBox = function (id) {
+    setMeme((meme) => {
+      return { ...meme, topText: [], bottomText: [] };
+    });
+
+    const newArray = allMemeImages;
+
+    for (let i = 0; i < newArray.length; i++) {
+      if (newArray[i].id === id) {
+        newArray[i].clicked = true;
+      }
+    }
+    setAllMemeImages(newArray);
   };
 
   // change text
@@ -69,10 +93,31 @@ const Meme = () => {
         ></input>
       </div>
 
-      <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
-        <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      <div className="grid--container">
+        {allMemeImages.map((el) => {
+          return (
+            <div
+              onClick={() => selectBox(el.id)}
+              key={el.id}
+              className="meme--box"
+              style={{
+                transform: el.clicked ? "scale(1.1)" : "",
+                backgroundColor: el.clicked ? "#d5d4d8" : "",
+              }}
+            >
+              <h4 className="meme--title">{el.name.toUpperCase()}</h4>
+              <div className="image--cont">
+                <img src={el.url} className="meme--image" />
+                {el.clicked && (
+                  <h2 className="meme--text top">{meme.topText}</h2>
+                )}
+                {el.clicked && (
+                  <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
